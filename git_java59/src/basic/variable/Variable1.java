b2ffcd97f18b59df2573c37b6fc85f3ce96b1a34
kimjoy70 <kimjoy70@hotmail.com>
2020-02-09 11:28:06 +0900

    Variable2 출력

---
+++ git_java59/src/basic/variable/Variable1.java
@@ -0,0 +1,9 @@
+package basic.variable;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		System.out.println("Variable1 실행");
+	}
+
+}
---
+++ git_java59/src/basic/variable/Variable2.java
@@ -0,0 +1,9 @@
+package basic.variable;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		System.out.println("Variable2 출력");
+	}
+
+}
